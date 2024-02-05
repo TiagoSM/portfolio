502744157280ceb5efb3b0bed7ceb9379c17aa8b
TiagoSM <tiagosantosmes@gmail.com>
2024-02-05 15:04:31 -0300

    Added button for mobile navigation and complete the 'Home' style

---
+++ script.js
@@ -0,0 +1,8 @@
+function toogleNav(){
+    let links = document.getElementById("links");
+    if (links.style.display === "block"){
+        links.style.display = "none";
+    } else {
+        links.style.display = "block";
+    }
+}
